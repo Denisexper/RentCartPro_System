e6ff9c6c17fadcfe498f5409663d485695383600
Denisexper <clasic.exper2023@gmail.com>
2026-04-19 19:31:43 -0600

    feat(auth): implement user registration with tenant association and password hashing

--- backend/src/controllers/auth/register.service.ts
+++ backend/src/controllers/auth/register.service.ts
@@ -1,7 +1,46 @@
 import prisma from "../../dataBase/prisma";
 import { Request, Response, NextFunction } from "express";
+import { registerInput } from "../../types/auth/auth.type";
+import { EncryptService } from "../../services/encrypt.service";
 
 export const registerControllerService = async (req: Request, res: Response, next: NextFunction) => {
     
+   const data: registerInput = req.body;
+   
+   try {
     
+    const userExist = await prisma.user.findUnique({
+        where: {email: data.email}
+    })
+
+    if(userExist){
+        return next({status: 400, message: "Email already exist"})
+    }
+
+    //hashear password
+    const hasPass = await EncryptService.hashPassword(data.password)
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: data.name,
+            email: data.email,
+            password: hasPass,
+            tenantId: data.tenantId,
+            role: 'Operator'
+        },
+        select: {
+            id: true,
+            email: true,
+            name: true,
+            tenantId: true,
+        }
+    })
+
+    return res.status(201).json({
+        msj: "User created successfully",
+        data: newUser
+    })
+   } catch (error) {
+    next(error)
+   }
 }
--- backend/src/types/auth/auth.type.ts
+++ backend/src/types/auth/auth.type.ts
@@ -2,3 +2,5 @@ import { User } from "@prisma/client"
 
 export type loginInput = Pick<User, 'password' |'email'>;
 
+
+export type registerInput = Pick<User, 'name' | 'email' | 'password' | 'tenantId'>;
