826ae6bb74869ef51f3cf9825dfb8ddbd7f8b398
Denisexper <clasic.exper2023@gmail.com>
2026-05-02 17:19:17 -0600

    refactor(backend): use enviroments.service vars in cloudinary config

--- backend/src/services/enviroments.service.ts
+++ backend/src/services/enviroments.service.ts
@@ -6,7 +6,10 @@ const requiredEnvVars = [
   "PORT",
   "DATABASE_URL",
   "JWT_SECRET",
-  "JWT_EXPIRES_IN"
+  "JWT_EXPIRES_IN",
+  "CLOUDINARY_CLOUD_NAME",
+  "CLOUDINARY_API_KEY",
+  "CLOUDINARY_API_SECRET"
 ];
 
 requiredEnvVars.forEach((env) => {
@@ -20,3 +23,6 @@ export const PORT = process.env.PORT;
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -1,10 +1,15 @@
 import { v2 as cloudinary } from "cloudinary";
 import { UploadApiResponse } from "cloudinary";
+import 
+{ CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET
+} from "../services/enviroments.service";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key:    process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key:    CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 });
 
 export const uploadBuffer = (buffer: Buffer, folder: string): Promise<UploadApiResponse> => {
